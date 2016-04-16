a1f24cc2bc8e6b08520430c6fb02c2774b5e50fe
Aperçu <bgronon@gmail.com>
2016-04-16 14:58:26 +0200

    refactor(lint): switch to eslint

--- test/index.js
+++ test/index.js
@@ -1,25 +1,26 @@
+'use strict';
 
 var Test = require('segmentio-integration-tester');
 var Sendwithus = require('../');
 var mapper = require('../lib/mapper');
 
-describe('Sendwithus', function(){
+describe('Sendwithus', function() {
   var sendwithus;
   var settings;
   var test;
 
-  beforeEach(function(){
-    //settings = { apiKey: 'key_segment1234abcd', integrationId: 'sio_segment1234abcd' };
+  beforeEach(function() {
+    // settings = { apiKey: 'key_segment1234abcd', integrationId: 'sio_segment1234abcd' };
     settings = {
       apiKey: 'live_7252c5d59aa9c0ed65239cce083ea7e5f0505f0f',
-      //apiKey: 'test_514d7517342c583abf9b73a617a529897014411a',
+      // apiKey: 'test_514d7517342c583abf9b73a617a529897014411a',
       integrationId: 'sio_MA2T2LtHDvsNDjDke7aC6Q' };
     sendwithus = new Sendwithus(settings);
     test = Test(sendwithus, __dirname);
     test.mapper(mapper);
   });
 
-  it('should have the correct settings', function(){
+  it('should have the correct settings', function() {
     test
       .name('Sendwithus')
       .channels(['client', 'server', 'mobile'])
@@ -28,34 +29,34 @@ describe('Sendwithus', function(){
       .retries(10);
   });
 
-  describe('.validate()', function(){
-    it('should not be valid without an api key', function(){
+  describe('.validate()', function() {
+    it('should not be valid without an api key', function() {
       delete settings.apiKey;
       delete settings.invalid;
       test.invalid({}, settings);
     });
 
-    it('should be valid with complete settings', function(){
+    it('should be valid with complete settings', function() {
       test.valid({}, settings);
     });
   });
 
-  describe('mapper', function(){
-    describe('identify', function(){
-      it('should map basic identify', function(){
+  describe('mapper', function() {
+    describe('identify', function() {
+      it('should map basic identify', function() {
         test.maps('identify-basic');
       });
     });
 
-    describe('track', function(){
-      it('should map basic track', function(){
+    describe('track', function() {
+      it('should map basic track', function() {
         test.maps('track-basic');
       });
     });
   });
 
-  describe('.identify()', function(){
-    it('should send basic identify', function(done){
+  describe('.identify()', function() {
+    it('should send basic identify', function(done) {
       var json = test.fixture('identify-basic');
       var output = json.output;
       output.timestamp = new Date(output.timestamp);
@@ -66,7 +67,7 @@ describe('Sendwithus', function(){
         .end(done);
     });
 
-    it('should not error on invalid key', function(done){
+    it('should not error on invalid key', function(done) {
       var json = test.fixture('identify-basic');
       var output = json.output;
       output.timestamp = new Date(output.timestamp);
@@ -78,8 +79,8 @@ describe('Sendwithus', function(){
     });
   });
 
-  describe('.track()', function(){
-    it('should send basic track', function(done){
+  describe('.track()', function() {
+    it('should send basic track', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
       output.timestamp = new Date(output.timestamp);
@@ -90,7 +91,7 @@ describe('Sendwithus', function(){
         .end(done);
     });
 
-    it('should not error on invalid key', function(done){
+    it('should not error on invalid key', function(done) {
       var json = test.fixture('track-basic');
       var output = json.output;
       output.timestamp = new Date(output.timestamp);
